870e98152770fb3d577642a50f190fdd89bf2dc2
Kevin McIntosh <73562909+kmac303@users.noreply.github.com>
2024-12-15 20:49:07 -0700

    updated lessons component layout

--- src/components/Lessons.jsx
+++ src/components/Lessons.jsx
@@ -6,14 +6,15 @@ function Lessons() {
         <section>
             <h1>LESSONS</h1>
             <div className="lesson-text-container">
-            <p>Want to get into VJing, but don’t know where to start? Are you already a VJ but want to step up your know-how?
-                <br /><br />
-                <strong>Unlock your visual creativity!</strong>
-                <br /><br />
-                Transform your live performances with expert VJ lessons designed for all skill levels. 
-                Learn the art of creating stunning visuals using tools like Resolume and Synesthesia Live. 
-                Whether youre just starting or looking to refine your craft, Visional’s tailored lessons will help you master the techniques and workflows to captivate any audience. 
-                <br /><br />
+            <p>Want to get into VJing, but don’t know where to start?<br />
+                Are you already a VJ but want to step up your know-how?<br />
+                <br />
+                <strong>Unlock your visual creativity!</strong><br />
+                <br />
+                Transform your live performances with expert VJ lessons designed for all skill levels.<br />
+                Learn the art of creating stunning visuals using tools like Resolume and Synesthesia Live.<br /> 
+                Whether you're just starting or looking to refine your craft, Visional’s tailored lessons will help you master the techniques and workflows to captivate any audience.<br />
+                <br />
                 <strong>Email contact@visionalmusic.com to book your session today and bring your visuals to life!</strong>
             </p>
             </div>
