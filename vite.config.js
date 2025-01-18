820c41756c13ff133a134dec51e41bc88ffd030a
Kevin McIntosh <73562909+kmac303@users.noreply.github.com>
2025-01-17 19:02:18 -0700

    rebuilt dist folder

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   define: {
     'import.meta.env.VITE_NODE_ENV': JSON.stringify('production')
