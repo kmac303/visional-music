858c79c97c86471a1bab0f3f7e36017c317ecdc6
Kevin McIntosh <73562909+kmac303@users.noreply.github.com>
2025-01-17 19:08:01 -0700

    fixed vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_NODE_ENV': JSON.stringify('production')
-  }
+  // define: {
+  //   'import.meta.env.VITE_NODE_ENV': JSON.stringify('production')
+  // }
 });
