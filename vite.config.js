5018eef5b7f39df805aad3885d768c7b3e073f78
Kevin McIntosh <73562909+kmac303@users.noreply.github.com>
2025-01-17 18:36:01 -0700

    removed react refresh

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   // plugins: [react()],
+//   plugins: import.meta.env.MODE === "development" ? [react()] : [],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'import.meta.env.VITE_NODE_ENV': JSON.stringify('production')
+  }
+});
