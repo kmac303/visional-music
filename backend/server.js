1c994dd64b3f11410daf923635eccf6aeabccbc2
Kevin McIntosh <73562909+kmac303@users.noreply.github.com>
2025-02-02 15:23:51 -0700

    updated server.js file to be more CORS friendly to play well with Safari

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,12 @@ const PORT = process.env.PORT || 5001;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*", // Allow all origins for now, but you can restrict it to your frontend URL
+    methods: ["POST", "GET"],
+    allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
 // Nodemailer Transporter Setup
