88e285ba0e772aa073777519ca4c035ba13b23a9
Kevin McIntosh <73562909+kmac303@users.noreply.github.com>
2025-02-02 18:06:15 -0700

    changed wording in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
             <h1>CONTACT</h1>
             <div className="contact-text-container">
                 <p>
-                    <strong>Want to book a VJ gig?
+                    <strong>Want to book a VJ set?
                     <br />
                     <Link to="/vjrequest" className="gig-request-link">
                     ➡ Fill out the VJ Request Form ⬅
